d5c879bdf192c4766d61bc37cad78a52787a9a4b
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-10-01 13:23:25 +0700

    Fix endpoint

--- web/src/services/auth.ts
+++ web/src/services/auth.ts
@@ -1,14 +1,15 @@
+import { serverEndpoint } from "@/config/endpoint";
 import { SiweMessage } from "siwe";
 
 export async function getNonce() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/nonce`, {
+  const res = await fetch(`${serverEndpoint}/auth/nonce`, {
     credentials: "include",
   });
   return res.json();
 }
 
 export async function verify(message: SiweMessage, signature: string) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/verify`, {
+  const res = await fetch(`${serverEndpoint}/auth/verify`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
