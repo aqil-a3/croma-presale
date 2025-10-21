b69be8814fd4c00c157749457e0151a1370af97e
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-10-21 11:17:25 +0700

    Fix Error

--- web/src/app/api/airdrop/create/route.ts
+++ web/src/app/api/airdrop/create/route.ts
@@ -1,16 +1,11 @@
 import { apiUser } from "@/services/db/users";
 import { NextRequest, NextResponse } from "next/server";
 
-
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const { createNewMigrationData } = apiUser;
 
-  try {
-    await createNewMigrationData(body);
-    return NextResponse.json({ message: "OK" });
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
+  await createNewMigrationData(body);
+
+  return NextResponse.json({ message: "OK" }, { status: 200 });
 }
